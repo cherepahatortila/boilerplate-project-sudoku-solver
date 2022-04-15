00bc0b9af7b973df211e96a6309a42a1038f7c57
Iryna <59018238+cherepahatortila@users.noreply.github.com>
2022-04-15 19:10:47 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,23 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
-
+    var {coordinate,value,puzzle}=req.body;
+    if(!coordinate||!puzzle)
+    return res.send({ error:'Required field(s) missing'});
+    let row=coordinate[0];
+    let col=coordinate[1];
+    if (coordinate.length>2)res.send({ error: 'Invalid coordinate'});
+    res.send(solver.checkValue(puzzle,row,col,value));
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+    
+    if(solver.validate(req.body.puzzle)==true){
+    console.log(solver.solve(req.body.puzzle))
+    return res.send(solver.solve(req.body.puzzle))}
+    else{
+      return res.send(solver.validate(req.body.puzzle))}
+      
     });
 };
